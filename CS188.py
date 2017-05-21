10e64b9f4e1ceeb8635d6ff0977dfeef76a20888
Dmaa <Dmaa@users.noreply.github.com>
2017-05-20 17:24:20 -0700

    Logistic Regression Actually working, trained on 50000 rows

--- CS188.py
+++ CS188.py
@@ -10,30 +10,32 @@ import pandas as pd
 import sklearn 
 import numpy 
 import matplotlib as plt 
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_auc_score
+from sklearn.cross_validation import StratifiedKFold
+from sklearn import linear_model, datasets
 
+rows_to_train = 50000
 data = pd.read_csv('image_data.csv', header=None) 
 
-print("Data loaded")
+print("Data loaded New")
 
 print(data.shape)
 data=data.dropna()
 print(data.shape)
 
-X=data.iloc[0:50000,0:622] 
-print ("Passed")
+X=data.iloc[0:rows_to_train,4:622] 
+print ("PassedNew")
 
-Y=data.iloc[0:500, 622]
-#rint("X shape")
+Y=data.iloc[0:rows_to_train, 622]
+X = X.as_matrix()
+Y= Y.as_matrix()
 
-#Y=data['Outcome']
+kfold = StratifiedKFold(y = Y, n_folds = 2, shuffle = True, random_state = 5)
 
-#print(X.shape)
-#print(Y.shape)
-#X=X.dropna()
-#Y=Y.dropna()
+for i, (train, test) in enumerate(kfold):
+    logreg = linear_model.LogisticRegression(C = 1e5)
+    logreg.fit(X[train], Y[train])
+    predictions = logreg.predict_proba(X[test])
 
-
-#X=X.as_matrix()
-#Y=Y.as_matrix()
-
-
+print("Passed2")
