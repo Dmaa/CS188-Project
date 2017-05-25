8ac062d39fe0c3f5e45ef04599c98424ae0b09b8
Vikash Singh <vikashsingh@s-164-67-209-115.resnet.ucla.edu>
2017-05-24 22:11:07 -0700

    With ROC evaluation

--- CS188.py
+++ CS188.py
@@ -8,15 +8,15 @@ Created on Wed May 10 23:19:43 2017
 #CS 188 Medical Imaging Project 
 import pandas as pd 
 import sklearn 
-import numpy 
+import numpy as np 
 import matplotlib as plt 
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import roc_auc_score
-from sklearn.cross_validation import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn import linear_model, datasets
 
 rows_to_train = 50000
-data = pd.read_csv('image_data.csv', header=None) 
+data = pd.read_csv('~/Desktop/cs188TD.csv', header=None) 
 
 print("Data loaded New")
 
@@ -28,17 +28,21 @@ X=data.iloc[0:rows_to_train,4:622]
 print ("PassedNew")
 
 Y=data.iloc[0:rows_to_train, 622]
+#convert to matrix to avoid errors 
 X = X.as_matrix()
 Y= Y.as_matrix()
 
-kfold = StratifiedKFold(y = Y, n_folds = 2, shuffle = True, random_state = 5)
+AUC=[]
+
+kfold = StratifiedKFold(y = Y, n_folds = 10, shuffle = True, random_state=3)
 
 for i, (train, test) in enumerate(kfold):
     logreg = linear_model.LogisticRegression(C = 1e5)
     logreg.fit(X[train], Y[train])
-    predictions = logreg.predict_proba(X[test])
+    predictionsproba=logreg.predict_proba(X[test])[:,1]
+    AUC.append(roc_auc_score(Y[test], predictionsproba)) 
     
-    
-print("Gittest")
 
-print("Passed2")
+
+print np.mean(AUC)
+
